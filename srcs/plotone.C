8a0ec51164f7c005207603710f89e1ea036f097c
Fran Nicolas <fjnicolas@ugr.es>
2023-02-20 09:48:45 -0600

    Firts commit

---
+++ srcs/plotone.C
@@ -0,0 +1,272 @@
+#define plotone_cxx
+#include "plotone.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+
+void plotone::Loop(int ichoice)
+{
+//   In a ROOT session, you can do:
+//      root> .L plotone.C
+//      root> plotone t
+//      root> t.GetEntry(12); // Fill t data members with entry number 12
+//      root> t.Show();       // Show values of entry 12
+//      root> t.Show(16);     // Read and show values of entry 16
+//      root> t.Loop();       // Loop on all entries
+//
+
+//     This is the loop skeleton where:
+//    jentry is the global entry number in the chain
+//    ientry is the entry number in the current Tree
+//  Note that the argument to GetEntry must be:
+//    jentry for TChain::GetEntry
+//    ientry for TTree::GetEntry and TBranch::GetEntry
+//
+//       To read only selected branches, Insert statements like:
+// METHOD1:
+//    fChain->SetBranchStatus("*",0);  // disable all branches
+//    fChain->SetBranchStatus("branchname",1);  // activate branchname
+// METHOD2: replace line
+//    fChain->GetEntry(jentry);       //read all branches
+//by  b_branchname->GetEntry(ientry); //read only this branch
+   if (fChain == 0) return;
+
+   TCanvas *c1=new TCanvas("c1","c1",1500,900);
+   c1->Divide(4,4); // divides the canvas into two rows and three columns
+
+
+   int nsamples = 2000;//15000;
+   /* if (fRun>2350 ) nsamples=15000; */
+   std::cout << " nsamples per waveform setting " << nsamples << " " << fRun << std::endl;
+
+   TH1F *wf[16];
+   for (int i=0;i<16;++i) {
+     TString title = Form("ch%02d",i);
+     wf[i] = new TH1F(title,title,nsamples,0,nsamples);
+     wf[i]->GetXaxis()->SetTitle("time (1 tick = 2 ns)");
+   }
+
+   /* Long64_t nentries = fChain->GetEntriesFast(); */
+   /* std::cout << nentries << std::endl; */
+
+    Long64_t nbytes = 0, nb = 0;
+   /* for (Long64_t jentry=0; jentry<nentries;jentry++) { */
+   Long64_t jentry = ichoice;
+   Long64_t ientry = LoadTree(jentry);
+   if (ientry < 0) return;
+   nb = fChain->GetEntry(jentry);   nbytes += nb;
+   // if (Cut(ientry) < 0) continue;
+
+   // make pictures of waveforms
+   //      std::cout << "Number of waveforms" << fWvfmsVec->size() << std::endl;
+   for (auto ich = 0;ich<fWvfmsVec->size();++ich) {
+     vector<unsigned short> thiswf = fWvfmsVec->at(ich);
+     if (jentry==0)	{ if (nsamples!=thiswf.size()) std::cout << "Number of samples set at " << nsamples <<
+							 " and waveforms are " << thiswf.size() << std::endl;}
+     //	std::cout << "Number of samples " << thiswf.size() << std::endl;
+     for (auto itick=0;itick<thiswf.size();++itick) {
+       wf[ich]->SetBinContent(itick+1,thiswf[itick]);
+     }
+     // determine if channel has signal
+
+     //  determine turn on time
+   }
+
+   //   wf[0]->SetTitle("counter 1");
+   //wf[0]->SetTitle("channel 0");
+//   wf[1]->SetTitle("Trigger");//channel 1");
+   //wf[2]->SetTitle("counter 1");
+   //wf[2]->SetTitle("channel 2");
+//   wf[3]->SetTitle("RWM");//channel 3");
+   //wf[4]->SetTitle("counter 2");
+   //wf[4]->SetTitle("channel 4");
+//   wf[5]->SetTitle("RWM");//channel 5");
+   //wf[6]->SetTitle("channel 6");
+//   wf[7]->SetTitle("RWM");//channel 7");
+   //wf[8]->SetTitle("counter 3");
+   //wf[8]->SetTitle("channel 8");
+//   wf[9]->SetTitle("RWM");//channel 9");
+   //wf[10]->SetTitle("counter 4");
+   //wf[10]->SetTitle("channel 10");
+//   wf[11]->SetTitle("RWM");//channel 11");
+   //wf[12]->SetTitle("counter 5");
+   //wf[12]->SetTitle("channel 12");
+//   wf[13]->SetTitle("RWM");//channel 13");
+   //wf[14]->SetTitle("channel 14");
+//   wf[15]->SetTitle("Trigger");//channel 15");
+   //wf[7]->SetTitle("event trigger");
+   //wf[14]->SetTitle("light trigger");
+   //wf[15]->SetTitle("RWM");
+
+   c1->cd(1);  wf[0]->Draw();
+   c1->cd(1);  wf[0]->Draw();
+   c1->cd(2);  wf[1]->Draw();
+   c1->cd(3);  wf[2]->Draw();
+   c1->cd(4);  wf[3]->Draw();
+   c1->cd(5);  wf[4]->Draw();
+   c1->cd(6);  wf[5]->Draw();
+   c1->cd(7);  wf[6]->Draw();
+   c1->cd(8);  wf[7]->Draw();
+   c1->cd(9);  wf[8]->Draw();
+   c1->cd(10);  wf[9]->Draw();
+   c1->cd(11);  wf[10]->Draw();
+   c1->cd(12);  wf[11]->Draw();
+   c1->cd(13);  wf[12]->Draw();
+   c1->cd(14);  wf[13]->Draw();
+   c1->cd(15);  wf[14]->Draw();
+   c1->cd(16);  wf[15]->Draw();
+   /*c1->cd(1);  wf[2]->Draw();
+   c1->cd(2);  wf[4]->Draw();
+   c1->cd(3);  wf[8]->Draw();
+   c1->cd(4);  wf[10]->Draw();
+   c1->cd(5);  wf[12]->Draw();
+   c1->cd(6);  wf[7]->Draw();
+   c1->cd(7);  wf[14]->Draw();
+   c1->cd(8);  wf[15]->Draw();*/
+
+      //      c1->Print(Form("../../6Desktop/PlotsSBND/Event%02d.png",(int)jentry));
+      //c1->Print(Form("Event%d.png",(int)jentry));
+
+}
+
+void plotone::Average()
+{
+//   In a ROOT session, you can do:
+//      root> .L plotone.C
+//      root> plotone t
+//      root> t.GetEntry(12); // Fill t data members with entry number 12
+//      root> t.Show();       // Show values of entry 12
+//      root> t.Show(16);     // Read and show values of entry 16
+//      root> t.Average();       // Loop on all entries and get average
+//
+
+//     This is the loop skeleton where:
+//    jentry is the global entry number in the chain
+//    ientry is the entry number in the current Tree
+//  Note that the argument to GetEntry must be:
+//    jentry for TChain::GetEntry
+//    ientry for TTree::GetEntry and TBranch::GetEntry
+//
+//       To read only selected branches, Insert statements like:
+// METHOD1:
+//    fChain->SetBranchStatus("*",0);  // disable all branches
+//    fChain->SetBranchStatus("branchname",1);  // activate branchname
+// METHOD2: replace line
+//    fChain->GetEntry(jentry);       //read all branches
+//by  b_branchname->GetEntry(ientry); //read only this branch
+   if (fChain == 0) return;
+
+   TCanvas *c1=new TCanvas("c1","c1",1500,900);
+   c1->Divide(1,2); // divides the canvas into two rows and three columns
+
+
+   int nsamples = 15000;
+   /* if (fRun>2350 ) nsamples=15000; */
+   std::cout << " nsamples per waveform setting " << nsamples << " " << fRun << std::endl;
+
+   TH1F *wf[16];
+   for (int i=0;i<16;++i) {
+     TString title = Form("ch%02d",i);
+     wf[i] = new TH1F(title,title,280,0,280);//nsamples,0,nsamples);
+     wf[i]->GetXaxis()->SetTitle("time around peak (1 tick=2 ns)");
+     wf[i]->SetBit(TH1::kIsAverage);
+   }
+
+   TH1F *wf_temp[16];
+   for (int i=0;i<16;++i) {
+     TString title_temp = Form("ch%02d",i);
+     wf_temp[i] = new TH1F(title_temp,title_temp,280,0,280);//nsamples,0,nsamples);
+     wf_temp[i]->GetXaxis()->SetTitle("time around peak (1 tick=2 ns)");
+     wf_temp[i]->SetBit(TH1::kIsAverage);
+   }
+
+    Long64_t nentries = fChain->GetEntriesFast();
+    std::cout << nentries << std::endl;
+
+    Long64_t nbytes = 0, nb = 0;
+ for (Long64_t jentry=0; jentry<nentries;jentry++) {
+   //Long64_t jentry = ichoice;
+   Long64_t ientry = LoadTree(jentry);
+   if (ientry < 0) return;
+   nb = fChain->GetEntry(jentry);   nbytes += nb;
+   // if (Cut(ientry) < 0) continue;
+
+
+   // make pictures of waveforms
+   //      std::cout << "Number of waveforms" << fWvfmsVec->size() << std::endl;
+   for (auto ich = 0;ich<fWvfmsVec->size();++ich) {
+     vector<unsigned short> thiswf = fWvfmsVec->at(ich);
+     if (jentry==0)	{ if (nsamples!=thiswf.size()) std::cout << "Number of samples set at " << nsamples <<
+							 " and waveforms are " << thiswf.size() << std::endl;}
+     //	std::cout << "Number of samples " << thiswf.size() << std::endl;
+     auto starttick=0;
+     for (auto itick=0;itick<thiswf.size();++itick) {
+       if (thiswf[itick]>4000) {
+         starttick=itick-100;
+         break;
+       }
+     }
+     if (starttick<0)starttick=0;
+     auto endtick = starttick+280;
+     if (endtick>thiswf.size()-1)endtick=thiswf.size()-1;
+     auto bintick = 1;
+     for (auto itick=starttick;itick<=endtick;++itick) {
+       wf_temp[ich]->SetBinContent(bintick,thiswf[itick]);
+       bintick++;
+     }
+     // determine if channel has signal
+     wf[ich]->Add(wf[ich],wf_temp[ich]);
+     wf_temp[ich]->Reset();
+     //  determine turn on time
+   }
+ }
+
+   //   wf[0]->SetTitle("counter 1");
+   wf[0]->SetTitle("channel 0");
+   wf[1]->SetTitle("channel 1");
+   wf[2]->SetTitle("counter 1");
+   wf[3]->SetTitle("channel 3");
+   wf[4]->SetTitle("counter 2");
+   wf[5]->SetTitle("channel 5");
+   wf[6]->SetTitle("channel 6");
+   //wf[7]->SetTitle("channel 7");
+   wf[8]->SetTitle("counter 3");
+   wf[9]->SetTitle("channel 9");
+   wf[10]->SetTitle("counter 4");
+   wf[11]->SetTitle("channel 11");
+   wf[12]->SetTitle("counter 5");
+   wf[13]->SetTitle("channel 13");
+   wf[7]->SetTitle("event trigger");
+   wf[14]->SetTitle("light trigger");
+   wf[15]->SetTitle("RWM");
+
+   //   c1->cd(1);  wf[0]->Draw();
+   c1->cd(1);  wf[0]->Draw();
+   c1->cd(2);  wf[1]->Draw();
+   //c1->cd(3);  wf[2]->Draw();
+   //c1->cd(4);  wf[3]->Draw();
+   //c1->cd(5);  wf[4]->Draw();
+   //c1->cd(6);  wf[5]->Draw();
+   //c1->cd(7);  wf[6]->Draw();
+   //c1->cd(8);  wf[7]->Draw();
+   //c1->cd(9);  wf[8]->Draw();
+   //c1->cd(10);  wf[9]->Draw();
+   //c1->cd(11);  wf[10]->Draw();
+   //c1->cd(12);  wf[11]->Draw();
+   //c1->cd(13);  wf[12]->Draw();
+   //c1->cd(14);  wf[13]->Draw();
+   //c1->cd(15);  wf[14]->Draw();
+   //c1->cd(16);  wf[15]->Draw();
+   /*c1->cd(1);  wf[2]->Draw();
+   c1->cd(2);  wf[4]->Draw();
+   c1->cd(3);  wf[8]->Draw();
+   c1->cd(4);  wf[10]->Draw();
+   c1->cd(5);  wf[12]->Draw();
+   c1->cd(6);  wf[7]->Draw();
+   c1->cd(7);  wf[14]->Draw();
+   c1->cd(8);  wf[15]->Draw();*/
+
+      //      c1->Print(Form("../../6Desktop/PlotsSBND/Event%02d.png",(int)jentry));
+      //c1->Print(Form("Event%d.png",(int)jentry));
+
+}
---
+++ srcs/plotone.h
@@ -0,0 +1,170 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Wed Oct  7 11:42:29 2020 by ROOT version 6.22/02
+// from TTree events/waveform tree
+// found on file: datafiles/caen1.root
+//////////////////////////////////////////////////////////
+
+#ifndef plotone_h
+#define plotone_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+
+// Header file for the classes stored in the TTree if any.
+#include "vector"
+
+class plotone {
+public :
+   TTree          *fChain;   //!pointer to the plotonelyzed TTree or TChain
+   TTree          *fChain2;
+   Int_t           fCurrent; //!current Tree number in a TChain
+
+// Fixed size dimensions of array or collections stored in the TTree if any.
+
+   // Declaration of leaf types
+   Int_t           fRun;
+   Int_t           fEvent;
+   vector<unsigned long> *fTicksVec;
+   vector<vector<unsigned short> > *fWvfmsVec;
+
+   Float_t art_ev;
+   Float_t caen_ev;
+   Float_t caenv_ev_tts;
+
+   // List of branches
+   TBranch        *b_fRun;   //!
+   TBranch        *b_fEvent;   //!
+   TBranch        *b_fTicksVec;   //!
+   TBranch        *b_fWvfmsVec;   //!
+
+   TBranch *b_art_ev;
+   TBranch *b_caen_ev;
+   TBranch *b_caenv_ev_tts;
+
+   plotone(TTree *tree=0, TTree *tree2=0);
+   virtual ~plotone();
+   virtual Int_t    Cut(Long64_t entry);
+   virtual Int_t    GetEntry(Long64_t entry);
+   virtual Long64_t LoadTree(Long64_t entry);
+   virtual void     Init(TTree *tree, TTree *tree2);
+   virtual void     Loop(int ichoice);
+   virtual void     Average();
+   virtual Bool_t   Notify();
+   virtual void     Show(Long64_t entry = -1);
+};
+
+#endif
+
+#ifdef plotone_cxx
+plotone::plotone(TTree *tree, TTree *tree2) : fChain(0)
+{
+// if parameter tree is not specified (or zero), connect the file
+// used to generate this class and read the Tree.
+   if (tree == 0) {
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("eventana_simdata_run4385.root");//caendump_run4484.root");//caenv1730dump_hist_run1291.root");
+      if (!f || !f->IsOpen()) {
+         f = new TFile("eventana_simdata_run4385.root");//caendump_run4484.root");//caenv1730dump_hist_run1291.root");
+      }
+      TDirectory * dir = (TDirectory*)f->Get("eventana_simdata_run4385.root:/dumpall");//caendump_run4484.root:/caenv1730dump");
+      dir->GetObject("events",tree);
+      dir->GetObject("nt_header",tree2);
+
+   }
+   Init(tree, tree2);
+}
+
+plotone::~plotone()
+{
+   if (!fChain || !fChain2) return;
+   delete fChain->GetCurrentFile();
+   delete fChain2->GetCurrentFile();
+}
+
+Int_t plotone::GetEntry(Long64_t entry)
+{
+// Read contents of entry.
+   if (!fChain || !fChain2) return 0;
+   return fChain->GetEntry(entry), fChain2->GetEntry(entry);
+}
+Long64_t plotone::LoadTree(Long64_t entry)
+{
+// Set the environment to read one entry
+   if (!fChain) return -5;
+   Long64_t centry = fChain->LoadTree(entry);
+   if (centry < 0) return centry;
+   if (fChain->GetTreeNumber() != fCurrent) {
+      fCurrent = fChain->GetTreeNumber();
+      Notify();
+   }
+   return centry;
+}
+
+void plotone::Init(TTree *tree, TTree *tree2)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the branch addresses and branch
+   // pointers of the tree will be set.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   // Set object pointer
+   fTicksVec = 0;
+   fWvfmsVec = 0;
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fCurrent = -1;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("fRun", &fRun, &b_fRun);
+   fChain->SetBranchAddress("fEvent", &fEvent, &b_fEvent);
+   fChain->SetBranchAddress("fTicksVec", &fTicksVec, &b_fTicksVec);
+   fChain->SetBranchAddress("fWvfmsVec", &fWvfmsVec, &b_fWvfmsVec);
+   Notify();
+
+   // Set object pointer
+   art_ev = 0;
+   caen_ev = 0;
+   caenv_ev_tts = 0;
+   // Set branch addresses and branch pointers
+   if (!tree2) return;
+   fChain2 = tree2;
+   fCurrent = -1;
+   fChain2->SetMakeClass(1);
+
+   fChain2->SetBranchAddress("art_ev", &art_ev, &b_art_ev);
+   fChain2->SetBranchAddress("caen_ev", &caen_ev, &b_caen_ev);
+   fChain2->SetBranchAddress("caenv_ev_tts", &caenv_ev_tts, &b_caenv_ev_tts);
+   Notify();
+}
+
+Bool_t plotone::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+void plotone::Show(Long64_t entry)
+{
+// Print contents of entry.
+// If entry is not specified, print current entry
+   if (!fChain) return;
+   fChain->Show(entry);
+}
+Int_t plotone::Cut(Long64_t entry)
+{
+// This function may be called from Loop.
+// returns  1 if entry is accepted.
+// returns -1 otherwise.
+   return 1;
+}
+#endif // #ifdef plotone_cxx
